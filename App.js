c89d0ee5eaef1cdc27356ce4847d0562ed3e84b3
Paul Smal <pavel.smal@gmail.com>
2018-05-29 02:12:38 +0300

    string length check with input and count

--- App.js
+++ App.js
@@ -6,54 +6,99 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions
+    Platform,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    Dimensions
 } from 'react-native';
 
 //The circle must be centered with a diameter of half the width of the screen.
 let diameter = Dimensions.get('window').width / 2
-const text = "“My goal is simple. It is a complete understanding of the universe, why it is as it is and why it exists at all”- Stephen Hawking"
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-            <Circle text='ttt' />
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '"My goal is simple.It is a complete understanding of the universe, why it is as it is and why it exists at all”- Stephen Hawking',
+        };
+    }
+
+    render() {
+        return (
+                <View style={styles.container}>
+                <Text style={styles.countText}>{this.state.text.length}</Text>
+                <Circle text={this.state.text} />
+                <UselessTextInput
+            multiline = {true}
+            numberOfLines = {4}
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}
+                />
+                </View>
+        );
+    }
 }
 
 class Circle extends Component {
     render() {
+        if(this.props.text.length < 140) {
+            return (
+                    <View style={styles.circle}>
+                    <Text style={styles.normalText}>{this.props.text}!</Text>
+                    </View>
+            );
+        } else {
+            return (
+                    <Text style={styles.warningText}>String length exceeded!</Text>
+            );
+        }
+    }
+}
+
+class UselessTextInput extends Component {
+    render() {
         return (
-        <View style={styles.circle}>
-                <Text style={styles.innertext}>{this.props.text}!</Text> 
-        </View>
+                <TextInput
+            {...this.props}
+            style={styles.textInput}
+            editable = {true}
+            maxLength = {150}
+                />
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000',
-  },
-  circle: {
-    width: diameter,
-    height: diameter,
-    borderRadius: diameter / 2 ,
-    backgroundColor: '#00BCD4',
-    justifyContent: "center",
-  },
-  innertext: {
-    color: '#fff',
-    textAlign: "center",
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#000',
+    },
+    circle: {
+        width: diameter,
+        height: diameter,
+        borderRadius: diameter / 2 ,
+        backgroundColor: '#00BCD4',
+        justifyContent: "center",
+    },
+    normalText: {
+        color: '#fff',
+        textAlign: "center",
+    },
+    warningText: {
+        color: 'red',
+        textAlign: "center",
+    },
+    countText: {
+        color: 'grey',
+        textAlign: "right",
+    },
+    textInput: {
+        color: 'grey',
+        textAlign: "center",
+    },
 });
